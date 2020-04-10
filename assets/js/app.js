fdb5389e23b0f4dacdf4b9666e82b6b5aad79a48
Mickd3 <mickaeld_03@hotmail.fr>
2020-04-10 12:08:57 +0200

    init project

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// any CSS you import will output into a single css file (app.css in this case)
+import '../css/app.css';
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+console.log('Hello Webpack Encore!! Edit me in assets/js/app.js');
+
+const App = () => {
+    return <h1>Hello World</h1>
+};
+
+const rootElement = document.querySelector('#app');
+ReactDOM.render(<App />, rootElement);
